476a33b41fe7c91898ce8188368ba562ff007f83
Ultra <ultra.public@proton.me>
2026-04-10 19:45:44 -0400

    Add initial navigation unittests

--- src/mission_executor/src/navigation.rs
+++ src/mission_executor/src/navigation.rs
@@ -16,6 +16,7 @@ pub fn get_new_point(
 
     let pos2 = Isometry3::identity();
 
+    //TODO: Instead of a segment, it should be two cuboids
     let contact = contact(cuboid_pos, &cuboid, &pos2, &segment, 0.0).unwrap()?;
 
     if target_point.coords.metric_distance(&contact.point1.coords) < CLOSE_ENOUGH
@@ -95,7 +96,8 @@ fn get_points(
 
         let world_face = cuboid_pos * face;
 
-        let translation = world_face * (dist + 2.5);
+        //TODO: This should not be a magic number. Also I'm not sure if this is totally safe either
+        let translation = world_face * (dist + 4.0);
         let point = midpoint + translation;
 
         let half_extents = map_bounds.size * 0.5;
@@ -120,5 +122,85 @@ fn get_dominant_axis(normal: &UnitVector3<f64>) -> usize {
         1
     } else {
         2
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use nalgebra::{Matrix3x1, Quaternion};
+
+    use super::*;
+    use crate::Pose;
+    fn setup(sub_pos: Matrix3x1<f64>, target_pos: Matrix3x1<f64>, cuboid_pos: Matrix3x1<f64>, cuboid_size: Matrix3x1<f64>, 
+        cuboid_rot: Quaternion<f64>) -> (BoundingBox3D, Isometry3<f64>, Segment, BoundingBox3D) {
+        let center = Pose::new(cuboid_pos, cuboid_rot);
+        let bbox = BoundingBox3D::new(center, cuboid_size);
+
+        let sub_point = Point3::from(sub_pos);
+        let target_point = Point3::from(target_pos); 
+        let segment = Segment::new(
+            sub_point,
+            target_point,
+        );
+
+        let unit_quaternion = UnitQuaternion::from_quaternion(Quaternion::identity());
+        let cuboid_pos = Isometry3::from_parts(
+            cuboid_pos.into(), unit_quaternion
+        );
+
+        let map_pos = Matrix3x1::zeros();
+        let map_pos = Pose::new(map_pos, Quaternion::identity());
+        let map_size = Matrix3x1::new(1000.0, 1000.0, 1000.0);
+
+        let map_bounds = BoundingBox3D::new(map_pos, map_size);
+
+        (bbox, cuboid_pos, segment, map_bounds)
+    }
+
+    fn collides(sub_point: Point3<f64>, bbox: BoundingBox3D, cuboid_pos: Isometry3<f64>, new_point: Point3<f64>) -> bool {
+        let test_segment = Segment::new(
+            sub_point,
+            new_point
+        );
+        let half_extents = bbox.size * 0.5;
+        let cuboid = Cuboid::new(half_extents);
+        let res =
+            contact(&cuboid_pos, &cuboid, &Isometry3::identity(), &test_segment, 0.0)
+            .expect("Should be supported");
+        res.is_some()
+    }
+
+    #[test]
+    fn no_collisions() {
+        let sub_pos = Matrix3x1::zeros();
+        let target_pos = Matrix3x1::new(5.0, 5.0, 5.0);
+        let cuboid_pos = Matrix3x1::new(-1.0, -1.0, -1.0);
+        let cuboid_size = Matrix3x1::new(1.0, 1.0, 1.0);
+        let rot = Quaternion::identity();
+
+        let (bbox, cuboid_pos, segment, map_bounds) = 
+            setup(sub_pos, target_pos, cuboid_pos, cuboid_size, rot);
+
+        let new_point = get_new_point(&bbox, &cuboid_pos, segment, &map_bounds);
+        assert!(new_point.is_none())
+    }
+
+    #[test]
+    fn simple_collision() {
+        let sub_pos = Matrix3x1::zeros();
+        let target_pos = Matrix3x1::new(5.0, 5.0, 5.0);
+        let cuboid_pos = Matrix3x1::new(2.0, 2.0, 2.0);
+        let cuboid_size = Matrix3x1::new(1.0, 1.0, 1.0);
+        let rot = Quaternion::identity();
+
+        let (bbox, cuboid_pos, segment, map_bounds) = 
+            setup(sub_pos, target_pos, cuboid_pos, cuboid_size, rot);
+
+        let new_point = get_new_point(&bbox, &cuboid_pos, segment, &map_bounds)
+            .expect("Collision didn't happen");
+        let point = Point3::new(2.25, 7.0, 2.0);
+        assert_eq!(point, new_point);
+
+        assert!(!collides(segment.a, bbox, cuboid_pos, new_point))
     }
 }
